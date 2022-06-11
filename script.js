ce452ba2efe5ff21cd4a8c84b355f9dc82bc8a01
eleoes <leo.escaroz@gmail.com>
2022-06-11 14:42:40 -0700

    search input works, removed id of char div

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const apiKey = "e00b87711d2f93def3b684538f72e256";
 const url = "https://gateway.marvel.com:443/v1/public/events/227/characters?orderBy=name&limit=91&apikey="+apiKey;
 
 // GLOBAL VARIABLES
-let currentList = [];
-let mainContainer = document.querySelector('#main-container');
+let characters = [];
+let filteredCharacters = [];
+const mainContainer = document.querySelector('#main-container');
 
 
 //GET DATA
@@ -16,77 +17,58 @@ function getData(){
         // .then(res => console.log(res.data.results))
         .then(res =>{
 
-            let characters = res.data.results;
-            currentList.push(characters);
-            
-            characters.forEach((character,index) =>{
-
-                // fetch character's name
-                let characterName = character.name;
-
-                // fetch character's image
-                let imgExtension = character.thumbnail.extension;
-                let imgPath = character.thumbnail.path;
-                let imgVariant = "portrait_medium";
-                let characterImg = imgPath+"/"+imgVariant+"."+imgExtension;
-
-                // create each character's div
-                let div = document.createElement('div');
-                div.setAttribute('id', `character-container-${index}`);
-                div.setAttribute('class', 'container');
-                div.innerHTML = `
-                    <div class = "character-image center">
-                        <img src = "${characterImg}" alt = "${characterName}">
-                    </div>
-                    <div class = "character-name">
-                        <h3>${characterName}</h3>
-                    </div>
-                `;
-
-                // append to mainContainer
-                mainContainer.appendChild(div);
-
-                
-                
-            });
-
+            characters.push(...res.data.results);
+            displayCharacters(characters);
+                        
         })
         .catch(error => console.log(error))
 };
 getData();
 
+function displayCharacters(results){
+    // clear main container
+    mainContainer.innerHTML = '';
+
+    //loop through each character's array
+    results.forEach((character,index) =>{
+
+        // fetch character's name
+        let characterName = character.name;
+
+        // fetch character's image
+        let imgExtension = character.thumbnail.extension;
+        let imgPath = character.thumbnail.path;
+        let imgVariant = "portrait_medium";
+        let characterImg = imgPath+"/"+imgVariant+"."+imgExtension;
+
+        // create each character's div
+        let div = document.createElement('div');
+        div.setAttribute('id', `character-container-${index}`);
+        div.setAttribute('class', 'character-container container');
+        div.innerHTML = `
+            <div class = "character-image center">
+                <img src = "${characterImg}" alt = "${characterName}">
+            </div>
+            <div class = "character-name">
+                <h3>${characterName}</h3>
+            </div>
+        `;
+
+        // append to mainContainer
+        mainContainer.appendChild(div);            
+    });
+}
+
 /* SEARCH BAR */
-let searchInput = document.querySelector('#search-input');
-searchInput.addEventListener("onkeydown", function(event){
-    filterList(event.target.value);
+const searchInput = document.querySelector('#search-input');
+
+searchInput.addEventListener("input", function(event){
+    filteredCharacters = characters.filter(char => {
+        return char.name.toLowerCase().includes(event.target.value.toLowerCase())
+    });
+    displayCharacters(filteredCharacters);
 });
 
 
-// searchInput.addEventListener("input", () => {
-//     setTimeout(function (){
-//         let searchResults = [];
-//         let characters = mainContainer;
-//         console.log(characters);
-//         // for (let i=0; i < characters.length; i++) {
-//         //     if(characters[i].name){
-//         //         if(characters[i].name.replaceAll('-', ' ').includes(document.getElementById('search-input').value.toLowerCase())){
-//         //             searchResults.push(characters[i]);
-//         //         };
-//         //     };
-//         // };
 
-//         // document.getElementById('main-container').innerHTML = '';
-//         // currentList = searchResults;
-//         // updateCharacterList();
-//     }, 1);
-
-// });
-
-// // function updateCharacterList(index) {
-// //     if(currentList[index]) {
-// //         mainContainer.insertAdjacentElement('beforeend', `<div class = "container center column" id = "character-container">`);
-            
-        
-// //     }
-// // }
 
